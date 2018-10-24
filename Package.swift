87b458f55ba66f20305341d9d138e47dbcc92da7
Matt Broadstone <mbroadst@gmail.com>
2018-10-24 12:05:56 -0400

    SWIFT-232: update module exported name to `bson`

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version:4.0
 import PackageDescription
 let package = Package(
-    name: "libbson",
+    name: "bson",
     pkgConfig: "libbson-1.0",
     providers: [
         .brew(["mongo-c-driver"]),
         .apt(["libbson-dev"])
     ],
     products: [
-        .library(name: "libbson", targets: ["libbson"])
+        .library(name: "bson", targets: ["bson"])
     ]
 )
