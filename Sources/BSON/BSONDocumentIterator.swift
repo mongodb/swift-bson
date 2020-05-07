90674e11a54b8c4e4c8520d75902592b60e6f76e
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-05-07 13:20:37 -0400

    rebase on master

--- Sources/BSON/BSONDocumentIterator.swift
+++ Sources/BSON/BSONDocumentIterator.swift
@@ -28,13 +28,25 @@ public struct BSONDocumentIterator: IteratorProtocol {
 
     /// Advances to the next element and returns it, or nil if no next element exists.
     public mutating func next() -> BSONDocument.KeyValuePair? {
+        do {
+            return try self._next()
+        } catch {
+            fatalError("\(error)")
+        }
+    }
+
+    /**
+     * Advances to the next element and returns it, or nil if no next element exists.
+     * - Throws:
+     */
+    private mutating func _next() throws -> BSONDocument.KeyValuePair? {
         guard self.buffer.readableBytes != 0 else {
             // Iteration has been exhausted
             return nil
         }
 
         guard let typeByte = self.buffer.readInteger(as: UInt8.self) else {
-            fatalError("BSONDocumentIterator Failed: Cannot read from \(self.buffer)")
+            throw BSONError.InternalError(message: "BSONDocumentIterator Failed: Cannot read from \(self.buffer)")
         }
 
         guard typeByte != 0 else {
@@ -43,31 +55,39 @@ public struct BSONDocumentIterator: IteratorProtocol {
         }
 
         guard let type = BSONType(rawValue: typeByte), type != .invalid else {
-            fatalError("BSONDocumentIterator Failed: Invalid type, \(typeByte)")
+            throw BSONError.InternalError(message: "BSONDocumentIterator Failed: Invalid type, \(typeByte)")
         }
 
-        do {
-            let key = try self.buffer.readCString()
-            guard let bson = try BSON.allBSONTypes[type]?.read(from: &buffer) else {
-                throw BSONError.InternalError(message: "Cannot read unknown type: \(type)")
-            }
-            return (key: key, value: bson)
-        } catch {
-            fatalError("BSONDocumentIterator.next() failed: \(error)")
+        let key = try self.buffer.readCString()
+        guard let bson = try BSON.allBSONTypes[type]?.read(from: &buffer) else {
+            throw BSONError.InternalError(message: "Cannot read unknown type: \(type)")
         }
+        return (key: key, value: bson)
     }
 
     /// Finds the key in the underlying buffer, and returns the [startIndex, endIndex) containing the corresponding
     /// element.
-    internal func findByteRange(for key: String) -> (startIndex: Int, endIndex: Int) {
-        fatalError("Unimplemented")
+    internal func findByteRange(for searchKey: String) -> (startIndex: Int, length: Int)? {
+        var iter = BSONDocumentIterator(over: self.buffer)
+        while true {
+            let starterIndex = iter.buffer.readerIndex
+            guard let (key, _) = iter.next() else {
+                // Iteration ended without finding a match
+                return nil
+            }
+            let enderIndex = iter.buffer.readerIndex
+
+            if key == searchKey {
+                return (startIndex: starterIndex, length: enderIndex - starterIndex)
+            }
+        }
     }
 }
 
 extension BSONDocument {
-    // this is an alternative to the built-in `Document.filter` that returns an `[KeyValuePair]`. this variant is
+    // this is an alternative to the built-in `BSONDocument.filter` that returns an `[KeyValuePair]`. this variant is
     // called by default, but the other is still accessible by explicitly stating return type:
-    // `let newDocPairs: [Document.KeyValuePair] = newDoc.filter { ... }`
+    // `let newDocPairs: [BSONDocument.KeyValuePair] = newDoc.filter { ... }`
     /**
      * Returns a new document containing the elements of the document that satisfy the given predicate.
      *
@@ -80,6 +100,10 @@ extension BSONDocument {
      * - Throws: An error if `isIncluded` throws an error.
      */
     public func filter(_ isIncluded: (KeyValuePair) throws -> Bool) rethrows -> BSONDocument {
-        fatalError("Unimplemented")
+        var elements: [BSONDocument.KeyValuePair] = []
+        for pair in self where try isIncluded(pair) {
+            elements.append(pair)
+        }
+        return BSONDocument(keyValuePairs: elements)
     }
 }
