6aff9669f86a44ef45132523580c0a5f204526bb
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-05-08 12:44:27 -0400

    Placeholder evg config

---
+++ .evergreen/run-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# variables
+PROJECT_DIRECTORY=${PROJECT_DIRECTORY:-$PWD}
+SWIFT_VERSION=${SWIFT_VERSION:-5.1}
+INSTALL_DIR="${PROJECT_DIRECTORY}/opt"
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+# enable swiftenv
+export SWIFTENV_ROOT="${INSTALL_DIR}/swiftenv"
+export PATH="${SWIFTENV_ROOT}/bin:$PATH"
+eval "$(swiftenv init -)"
+
+# select the latest Xcode for Swift 5.1 support on MacOS
+if [ "$OS" == "darwin" ]; then
+    sudo xcode-select -s /Applications/Xcode11.3.app
+fi
+
+# switch swift version, and run tests
+swiftenv local $SWIFT_VERSION
+
+# build the driver
+swift build
+
+# test the driver
+swift test
