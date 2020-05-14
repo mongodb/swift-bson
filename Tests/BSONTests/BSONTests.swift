ff1346bfa7147dd5b8bbe52e1cc6cb3040f28469
Neal Beeken <neal.beeken@mongodb.com>
2020-05-13 21:56:57 -0400

    feat: bson document, int32, and int64
    Also adds support for document iteration and subscripting
    Simple testing of each of the mentioned features

--- Tests/BSONTests/BSONTests.swift
+++ Tests/BSONTests/BSONTests.swift
@@ -1,1 +1,45 @@
 import BSON
+import Nimble
+import XCTest
+
+open class BSONTestCase: XCTestCase {
+    /// Helpers, but nothing yet...
+}
+
+public extension Document {
+    func toByteString() -> String {
+        guard let bytes = self.buffer.getBytes(at: 0, length: self.buffer.readableBytes) else {
+            return ""
+        }
+        var string = ""
+        for byte in bytes {
+            if (33 < byte) && (byte < 126) {
+                string += String(UnicodeScalar(byte))
+            } else {
+                string += "\\x" + String(format: "%02X", byte)
+            }
+        }
+        return string
+    }
+
+    func readAllBytes() -> [UInt8] {
+        guard let bytes = self.buffer.getBytes(at: 0, length: self.buffer.readableBytes) else {
+            return []
+        }
+        return bytes
+    }
+}
+
+public extension Array where Element == UInt8 {
+    func toByteString() -> String {
+        var string = ""
+        for byte in self {
+            if (33 < byte) && (byte < 126) {
+                string += String(UnicodeScalar(byte))
+            } else {
+                string += "\\x" + String(format: "%02X", byte)
+            }
+        }
+        return string
+    }
+}
