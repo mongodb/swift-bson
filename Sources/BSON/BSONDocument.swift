2835379ded9686f9feb5d68175bc63a77eda98f2
Neal Beeken <neal.beeken@mongodb.com>
2020-06-15 18:03:46 -0400

    Document size requirement

--- Sources/BSON/BSONDocument.swift
+++ Sources/BSON/BSONDocument.swift
@@ -93,7 +93,8 @@ public struct BSONDocument {
      * Initializes a new `BSONDocument` from the provided BSON data.
      *
      * - Throws:
-     *   - `InvalidArgumentError` if the data passed is invalid BSON
+     *   - `InvalidArgumentError` if the data passed is invalid BSON,
+     *      ByteBuffer must have `readableBytes` equal to the BSON's leading size indicator
      *
      * - SeeAlso: http://bsonspec.org/
      */
