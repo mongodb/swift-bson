0817299a9ee8d0f3c30c228489fa45ff9108d0a5
Neal Beeken <neal.beeken@mongodb.com>
2020-06-16 13:26:56 -0400

    Test fix sourcery file passes

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,5 +1,23 @@
 @testable import BSONTests
 import XCTest
 
+extension BSONCorpusTests {
+    static var allTests = [
+        ("testBSONCorpus", testBSONCorpus),
+    ]
+}
+
+extension DocumentTests {
+    static var allTests = [
+        ("testCount", testCount),
+        ("testKeys", testKeys),
+        ("testValues", testValues),
+        ("testSubscript", testSubscript),
+        ("testDynamicMemberLookup", testDynamicMemberLookup),
+    ]
+}
+
 XCTMain([
+    testCase(BSONCorpusTests.allTests),
+    testCase(DocumentTests.allTests),
 ])
