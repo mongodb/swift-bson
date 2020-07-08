f59109d131f2f726545e11a3194dc2838786c145
Patrick Freed <patrick.freed@mongodb.com>
2020-07-08 15:54:09 -0400

    add macos platform

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "swift-bson",
+    platforms: [
+        .macOS(.v10_14)
+    ],
     products: [
         .library(name: "BSON", targets: ["BSON"])
     ],
