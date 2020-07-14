5545d0277ed1eab990c59fc529527a2e1d67de90
Nellie Spektor <nellie.spektor@gmail.com>
2020-07-14 15:13:22 -0400

    doc change

--- Sources/BSON/ExtendedJSONDecoder.swift
+++ Sources/BSON/ExtendedJSONDecoder.swift
@@ -11,8 +11,8 @@ public class ExtendedJSONDecoder {
     /// - Parameters:
     ///   - type: Codable type to decode the input into.
     ///   - data: `Data` which represents the JSON that will be decoded.
-    /// - Returns: Decoded representation of the JSON input as an instance of T.
-    /// - Throws:
+    /// - Returns: Decoded representation of the JSON input as an instance of `T`.
+    /// - Throws: `DecodingError` if the JSON data is corrupt or if any value throws an error during decoding.
     public func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T {
         // Takes in JSON as `Data` encoded with `.utf8` and runs it through a `JSONDecoder` to get an
         // instance of the `JSON` enum. Then a `BSON` enum instance is created via the `JSON`.
