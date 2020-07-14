0304474ee9330fc0e7332e4dc43f3b3630b2645a
Nellie Spektor <nellie.spektor@gmail.com>
2020-07-14 14:06:57 -0400

    Documentation Updates

--- Sources/BSON/ExtendedJSONDecoder.swift
+++ Sources/BSON/ExtendedJSONDecoder.swift
@@ -1,23 +1,23 @@
 import Foundation
-/// Decoder for ExtendedJSON
+/// `ExtendedJSONDecoder` facilitates the decoding of ExtendedJSON into `Decodable` values.
 public class ExtendedJSONDecoder {
-    /// Initialize a Decoder
+    /// Initialize an `ExtendedJSONDecoder`.
     public init() {
         fatalError("unimplemented")
     }
 
-    /// Takes in JSON as `Data` encoded with `.utf8` and runs it through a `JSONDecoder` to get an
-    /// instance of the `JSON` enum. Then a `BSON` enum instance is created via the `JSON`.
-    /// The `BSON is then passed through a `BSONDecoder` where it is outputted as a `T`
-    ///
-    /// Data --> JSON --> BSON --> T
+    /// Decodes an instance of the requested type `T` from the provided extended JSON data.
     ///
     /// - Parameters:
     ///   - type: Codable type to decode the input into.
-    ///   - data: `Data` which represents the `JSON` that will be decoded.
-    /// - Returns: Decoded representation of the `JSON` input as an instance of T.
+    ///   - data: `Data` which represents the JSON that will be decoded.
+    /// - Returns: Decoded representation of the JSON input as an instance of T.
     /// - Throws:
     public func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T {
+        // Takes in JSON as `Data` encoded with `.utf8` and runs it through a `JSONDecoder` to get an
+        // instance of the `JSON` enum. Then a `BSON` enum instance is created via the `JSON`.
+        // The `BSON` is then passed through a `BSONDecoder` where it is outputted as a `T`
+        // Data --> JSON --> BSON --> T
         fatalError("unimplemented")
     }
 }
