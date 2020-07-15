e92f308df30550c9dbeeda61f8e8c9707c1689bd
Nellie Spektor <nellie.spektor@gmail.com>
2020-07-15 10:16:58 -0400

    SWIFT-917 Add sketch of ExtendedJSONDecoder public API

---
+++ Sources/BSON/ExtendedJSONDecoder.swift
@@ -0,0 +1,24 @@
+import Foundation
+/// `ExtendedJSONDecoder` facilitates the decoding of ExtendedJSON into `Decodable` values.
+public class ExtendedJSONDecoder {
+    /// Initialize an `ExtendedJSONDecoder`.
+    public init() {
+        fatalError("unimplemented")
+    }
+
+    /// Decodes an instance of the requested type `T` from the provided extended JSON data.
+    /// - SeeAlso: https://docs.mongodb.com/manual/reference/mongodb-extended-json/
+    ///
+    /// - Parameters:
+    ///   - type: Codable type to decode the input into.
+    ///   - data: `Data` which represents the JSON that will be decoded.
+    /// - Returns: Decoded representation of the JSON input as an instance of `T`.
+    /// - Throws: `DecodingError` if the JSON data is corrupt or if any value throws an error during decoding.
+    public func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T {
+        // Takes in JSON as `Data` encoded with `.utf8` and runs it through a `JSONDecoder` to get an
+        // instance of the `JSON` enum. Then a `BSON` enum instance is created via the `JSON`.
+        // The `BSON` is then passed through a `BSONDecoder` where it is outputted as a `T`
+        // Data --> JSON --> BSON --> T
+        fatalError("unimplemented")
+    }
+}
