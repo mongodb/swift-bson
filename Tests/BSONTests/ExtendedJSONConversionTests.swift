f31c8e2df300c4ff7ece3212352bf93ce25ad7e4
Patrick Freed <patrick.freed@mongodb.com>
2020-08-18 15:36:58 -0400

    fix test

--- Tests/BSONTests/ExtendedJSONConversionTests.swift
+++ Tests/BSONTests/ExtendedJSONConversionTests.swift
@@ -338,8 +338,18 @@ open class ExtendedJSONConversionTestCase: BSONTestCase {
         expect(try BSONDocument(fromJSON: "{\"key\": {\"$numberInt\": \"5\"}}".data(using: .utf8)!))
             .to(equal(["key": .int32(5)]))
 
-        let canonicalExtJSON = "{\"key\":{\"$numberInt\":\"5\"}}"
-        let relaxedExtJSON = "{\"key\":5}"
+        let canonicalExtJSON = """
+        {
+          "key" : {
+            "$numberInt" : "5"
+          }
+        }
+        """
+        let relaxedExtJSON = """
+        {
+          "key" : 5
+        }
+        """
         let canonicalDoc = try BSONDocument(fromJSON: canonicalExtJSON)
         let relaxedDoc = try BSONDocument(fromJSON: relaxedExtJSON)
         expect(canonicalDoc).to(equal(["key": .int32(5)]))
