fab5b4db47631e176796455ee88fa24f4cd456a0
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-08-26 13:12:47 -0400

    Add version string check

--- etc/release.sh
+++ etc/release.sh
@@ -6,14 +6,18 @@
 set -e
 
 # ensure we are on master before releasing
-git checkout master
+#git checkout master
+
+version=${1}
+# Ensure version is non-empty
+[ ! -z "${version}" ] || { echo "ERROR: Missing version string"; exit 1; }
 
 # regenerate documentation with new version string
-./etc/generate-docs.sh ${1}
+./etc/generate-docs.sh ${version}
 
 # tag release and push tag
-git tag "v${1}"
+git tag "v${version}"
 git push --tags
 
 # go to GitHub to publish release notes
-open "https://github.com/mongodb/swift-bson/releases/tag/v${1}"
+open "https://github.com/mongodb/swift-bson/releases/tag/v${version}"
