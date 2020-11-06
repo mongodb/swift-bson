a38a34dd141e7e1d96dd2d949102a189a36eb2da
Kaitlin Mahar <kaitlinmahar@gmail.com>
2020-11-05 19:01:52 -0500

    Convert test output to XML on Evergreen

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
@@ -7,6 +7,8 @@ PROJECT_DIRECTORY=${PROJECT_DIRECTORY:-$PWD}
 SWIFT_VERSION=${SWIFT_VERSION:-5.2.4}
 INSTALL_DIR="${PROJECT_DIRECTORY}/opt"
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+RAW_TEST_RESULTS="${PROJECT_DIRECTORY}/rawTestResults"
+XML_TEST_RESULTS="${PROJECT_DIRECTORY}/testResults.xml"
 
 # enable swiftenv
 export SWIFTENV_ROOT="${INSTALL_DIR}/swiftenv"
@@ -24,5 +26,19 @@ swiftenv local $SWIFT_VERSION
 # build the driver
 swift build
 
+# even if tests fail we want to parse the results, so disable errexit
+set +o errexit
+# propagate error codes in the following pipes
+set -o pipefail
+
 # test the driver
-swift test --enable-test-discovery
+swift test --enable-test-discovery 2>&1 | tee ${RAW_TEST_RESULTS}
+
+# save tests exit code
+EXIT_CODE=$?
+
+# convert tests to XML
+cat ${RAW_TEST_RESULTS} | swift "${PROJECT_DIRECTORY}/etc/convert-test-results.swift" > ${XML_TEST_RESULTS}
+
+# exit with exit code for running the tests
+exit $EXIT_CODE
