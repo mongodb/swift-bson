23cfe16ff8bac69f2a75414f897a6a3d5aab3e77
Patrick Freed <patrick.freed@mongodb.com>
2021-01-12 16:41:58 -0500

    remove JSON fragment workaround

--- Tests/SwiftBSONTests/JSONTests.swift
+++ Tests/SwiftBSONTests/JSONTests.swift
@@ -12,33 +12,29 @@ open class JSONTestCase: XCTestCase {
     func testInteger() throws {
         // Initializing a JSON with an int works, but it will be cast to a double.
         let intJSON: JSON = 12
-        let encoded = Data(try encoder.encode([intJSON]))
-        /* JSONEncoder currently cannot encode non-object/array top level values.
-         To get around this, the generated JSON will need to be wrapped in an array
-         and unwrapped again at the end as a workaround.
-         This workaround can be removed when Swift 5.3 is the minimum supported version by the BSON library. */
-        expect(Double(String(data: encoded.dropFirst().dropLast(), encoding: .utf8)!)!)
+        let encoded = Data(try encoder.encode(intJSON))
+        expect(Double(String(data: encoded, encoding: .utf8)!)!)
             .to(beCloseTo(12))
     }
 
     func testDouble() throws {
         let doubleJSON: JSON = 12.3
-        let encoded = Data(try encoder.encode([doubleJSON]))
-        expect(Double(String(data: encoded.dropFirst().dropLast(), encoding: .utf8)!)!)
+        let encoded = Data(try encoder.encode(doubleJSON))
+        expect(Double(String(data: encoded, encoding: .utf8)!)!)
             .to(beCloseTo(12.3))
     }
 
     func testString() throws {
         let stringJSON: JSON = "I am a String"
-        let encoded = Data(try encoder.encode([stringJSON]))
-        expect(String(data: encoded.dropFirst().dropLast(), encoding: .utf8))
+        let encoded = Data(try encoder.encode(stringJSON))
+        expect(String(data: encoded, encoding: .utf8))
             .to(equal("\"I am a String\""))
     }
 
     func testBool() throws {
         let boolJSON: JSON = true
-        let encoded = Data(try encoder.encode([boolJSON]))
-        expect(String(data: encoded.dropFirst().dropLast(), encoding: .utf8))
+        let encoded = Data(try encoder.encode(boolJSON))
+        expect(String(data: encoded, encoding: .utf8))
             .to(equal("true"))
     }
 
