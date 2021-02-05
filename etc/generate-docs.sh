6aeef24ee3e25db10eeb3e1f223a947a24e1e78b
Patrick Freed <patrick.freed@mongodb.com>
2021-02-05 17:09:11 -0500

    SWIFT-734 Update scripts to maintain multiple versions of the documentation (#58)

--- etc/generate-docs.sh
+++ etc/generate-docs.sh
@@ -9,32 +9,25 @@ if ! command -v jazzy > /dev/null; then
   gem install jazzy || { echo "ERROR: Failed to locate or install jazzy; please install yourself with 'gem install jazzy' (you may need to use sudo)"; exit 1; }
 fi
 
+if ! command -v sourcekitten > /dev/null; then
+  echo "ERROR: Failed to locate SourceKitten; please install yourself and/or add to your \$PATH"; exit 1
+fi
+
 version=${1}
 
 # Ensure version is non-empty
 [ ! -z "${version}" ] || { echo "ERROR: Missing version string"; exit 1; }
 
-jazzy \
---github-file-prefix https://github.com/mongodb/swift-bson/tree/v${version} \
---module-version "${version}" \
---output "docs-temp/SwiftBSON" \
---config .jazzy.yml
+jazzy_args=(--clean
+            --github-file-prefix https://github.com/mongodb/swift-bson/tree/v${version} 
+            --module-version "${version}")
 
-# switch to docs branch to commit and push
-git stash
-git checkout gh-pages
+sourcekitten doc --spm --module-name SwiftBSON > swift-bson-docs.json
+args=("${jazzy_args[@]}"  --output "docs-temp/SwiftBSON" --module "SwiftBSON" --config ".jazzy.yml" 
+        --sourcekitten-sourcefile swift-bson-docs.json
+        --root-url "https://mongodb.github.io/swift-bson/docs/SwiftBSON/")
+jazzy "${args[@]}"
 
-rm -rf docs/*
-cp -r docs-temp/* docs/
-rm -rf docs-temp
+echo '<html><head><meta http-equiv="refresh" content="0; url=SwiftBSON/index.html" /></head></html>' > docs-temp/index.html
 
-git add docs/
-
-echo '<html><head><meta http-equiv="refresh" content="0; url=BSON/index.html" /></head></html>' > docs/index.html
-git add docs/index.html
-
-git commit -m "${version} docs"
-git push
-
-# go back to wherever we started
-git checkout -
+rm swift-bson-docs.json
