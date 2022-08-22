fb491f3e1ffa28165e1595ef0a2f666662eb56dc
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2022-08-22 18:29:42 -0400

    SWIFT-1337 drop Swift 5.1 support  (#86)

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -19,8 +19,14 @@ if [ "$SANITIZE" != "false" ]; then
     SANITIZE_STATEMENT="--sanitize ${SANITIZE}"
 fi
 
+# work around https://github.com/mattgallagher/CwlPreconditionTesting/issues/22 (bug still exists in version 1.x
+# when using Xcode 13.2)
+if [ "$OS" == "darwin" ]; then
+    EXTRA_FLAGS="-Xswiftc -Xfrontend -Xswiftc -validate-tbd-against-ir=none"
+fi
+
 # build the driver
-swift build $SANITIZE_STATEMENT
+swift build $EXTRA_FLAGS $SANITIZE_STATEMENT
 
 # even if tests fail we want to parse the results, so disable errexit
 set +o errexit
@@ -28,7 +34,7 @@ set +o errexit
 set -o pipefail
 
 # test the driver
-swift test --enable-test-discovery $SANITIZE_STATEMENT 2>&1 | tee ${RAW_TEST_RESULTS}
+swift test --enable-test-discovery $EXTRA_FLAGS $SANITIZE_STATEMENT 2>&1 | tee ${RAW_TEST_RESULTS}
 
 # save tests exit code
 EXIT_CODE=$?
